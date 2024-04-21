ddcb347263225e2456b98a58f3963299493179c9
elAleDeArg <alevillaelisa555@gmail.com>
2024-04-20 21:05:19 -0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Nav from './Nav'
+import {BrowserRouter} from 'react-router-dom'
 
 const App = () => {
   return (
+    <BrowserRouter>
     <Nav />
+    </BrowserRouter>
+    
   )
 }
 
--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { FaTruck, FaHeart, FaShoppingCart, FaUser } from "react-icons/fa";
+import { RiLoginBoxFill, RiLogoutBoxFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+
 import "./Nav.css";
 
 const Nav = () => {
+  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
   return (
     <>
       <div className="free">
@@ -28,16 +33,68 @@ const Nav = () => {
             <button>Buscar</button>
           </div>
           <div className="icon">
-            <div className="account">
+            {
+                isAuthenticated &&
+                (<div className="account">
               <div className="user_icon">
                 <FaUser />
               </div>
-              <p>Hola, usuario</p>
-            </div>
+              <p>Hola, {user.name}</p>
+            </div>)
+            }
+            
             <div className="second_icon">
-              <p><FaHeart /></p>
-              <p><FaShoppingCart /></p>
+              <Link to="/" className="link">
+                <FaHeart />
+              </Link>
+              <Link to="/cart" className="link">
+                <FaShoppingCart />
+              </Link>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="header">
+        <div className="container">
+          <div className="nav">
+            <ul>
+              <li>
+                <Link to="/" className="link">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/products" className="link">
+                  Obras
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" className="link">
+                  Acerca de mi
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact" className="link">
+                  Contacto
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="auth">
+            {isAuthenticated ? (
+              <button
+                onClick={() =>
+                  logout({ logoutParams: { returnTo: window.location.origin } })
+                }
+              >
+                <RiLogoutBoxFill />
+              </button>
+            ) : (
+              <button onClick={() => loginWithRedirect()}>
+                <RiLoginBoxFill />
+              </button>
+            )}
           </div>
         </div>
       </div>
